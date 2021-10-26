07f74d7f3f8976dfcec7de027ddd59da65c850a0
red <rdagnew@usc.edu>
2021-10-26 16:27:39 -0700

    check support for CN=1

--- src/hmmcnc.cpp
+++ src/hmmcnc.cpp
@@ -827,7 +827,7 @@ void mergeIntervals(vector<Interval> & intervals, vector<Interval> &mergedInterv
   }
 }
 
-void intersectDelCall( vector<Interval> &mergedIntervals, vector<Interval> & copyIntervals)
+void intersectDelCall( vector<Interval> &mergedIntervals, vector<Interval> & copyIntervals, double mean)
 {
  
   int i = 0, j = 0;
@@ -856,6 +856,12 @@ void intersectDelCall( vector<Interval> &mergedIntervals, vector<Interval> & cop
         copyIntervals[j].end = mergedIntervals[i].end;
         copyIntervals[j].altInfo += ":DF";
         copyIntervals[j].altSample += ":1" ;
+
+        if (((double)copyIntervals[j].averageCoverage) > (mean/4.0) ){
+          copyIntervals[j].copyNumber = 1;
+
+        }
+        else{copyIntervals[j].copyNumber = 0;}
         i++;j++;
       }
       else if (mergedIntervals[i].end < copyIntervals[j].end)
@@ -2489,7 +2495,7 @@ int hmcnc(Parameters& params) {
 
     mergeIntervals(delT[c], MdelT[c], contigNames[c] );
 
-    intersectDelCall(MdelT[c], copyIntervals[c] );
+    intersectDelCall(MdelT[c], copyIntervals[c] , mean);
 
 
   }
